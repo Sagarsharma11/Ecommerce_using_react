83f837cc5f89cef05072a3f91a7c0103a0dcca13
fx900-fx97@7 <ss365493@gmail.com>
2022-12-21 23:10:57 +0530

    adding multer , axios and send the file using FormData

--- mysite/backend/routes/admin.js
+++ mysite/backend/routes/admin.js
@@ -2,12 +2,26 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const product = require('../modules/product')
+const multer  = require('multer')
 
-router.post('/product' , async(req,res)=>{
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'C:/Users/sagar/Desktop/Ecommerce_using_react/mysite/public/images')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, uniqueSuffix+"-"+file.originalname )
+    }
+  })
+
+const upload = multer({ storage : storage})
+
+router.post('/product' ,upload.single('productimage'), async(req,res)=>{
     try {
         var prod = await product.create({
             productname:req.body.productname,
-            productimage:req.body.productimage,
+            productimage:req.file.filename,
             productprice:req.body.productprice,
             productoffer:req.body.productoffer       
         })
--- mysite/src/App.js
+++ mysite/src/App.js
@@ -3,24 +3,25 @@ import Signup from "./Component/Signup";
 import Login from "./Component/Login";
 import Home from "./Component/Home";
 import Main from "./Dashbord/Main";
-
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
+import AddProduct from "./Dashbord/AddProduct";
 
 function App() {
   return (
     <>
-      <Router>
+    
         <Routes>
          <Route exact path="/" element={<Login/>} />
           <Route exact path="/signup" element={<Signup />} />
          <Route exact path="/home" element={<Home/>} />
           <Route exact path="/admin" element={<Main/>}/>
+          <Route exact path="/addproduct" element={<AddProduct/>}/>
         </Routes>
-      </Router>
+  
 
     </>
   );
